46432310ddf4e7320a25664d18dfb7af28a244be
Christian Vadalà <chr.vadala@gmail.com>
2015-05-29 18:23:59 +0200

    attiva stampe

--- lastfm/src/org/yottabase/lastfm/noop/NoopDriver.java
+++ lastfm/src/org/yottabase/lastfm/noop/NoopDriver.java
@@ -13,27 +13,27 @@ public class NoopDriver implements Driver{
 	
 	@Override
 	public void insertUser(User user) {
-//		System.out.println("code: " + user.getCode());
-//		System.out.println("gender: "+ user.getGender());
-//		System.out.println("age: " + user.getAge());
-//		System.out.println("country: " + user.getCountry());
-//		
-//		System.out.println("==========================");
+		System.out.println("code: " + user.getCode());
+		System.out.println("gender: "+ user.getGender());
+		System.out.println("age: " + user.getAge());
+		System.out.println("country: " + user.getCountry());
+		
+		System.out.println("==========================");
 		
 	}
 
 	@Override
 	public void insertListenedTrack(ListenedTrack listenedTrack) {
 		
-//		System.out.println("code: " + listenedTrack.getCode());
-//		System.out.println("time: " + listenedTrack.getTime().toString());
-//		System.out.println("artistId: " + listenedTrack.getArtistCode());
-//		System.out.println("artistName: " + listenedTrack.getArtistName());
-//		System.out.println("trackId: " + listenedTrack.getTrackCode());
-//		System.out.println("trackName: " + listenedTrack.getTrackName());
-//		
-//		System.out.println("==========================");
-//		
+		System.out.println("code: " + listenedTrack.getCode());
+		System.out.println("time: " + listenedTrack.getTime().toString());
+		System.out.println("artistId: " + listenedTrack.getArtistCode());
+		System.out.println("artistName: " + listenedTrack.getArtistName());
+		System.out.println("trackId: " + listenedTrack.getTrackCode());
+		System.out.println("trackName: " + listenedTrack.getTrackName());
+		
+		System.out.println("==========================");
+		
 	}
 	
 }
