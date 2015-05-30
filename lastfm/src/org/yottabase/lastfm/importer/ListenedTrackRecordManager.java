576545ae1a847745a21ce3c9d7977e049cbe060a
hduser <hduser@leonardo-K53SD>
2015-05-30 19:03:45 +0200

    Aggiunge codice artista e canzone dove mancante

--- lastfm/src/org/yottabase/lastfm/importer/ListenedTrackRecordManager.java
+++ lastfm/src/org/yottabase/lastfm/importer/ListenedTrackRecordManager.java
@@ -9,38 +9,42 @@ import java.util.Iterator;
 import java.util.List;
 
 public class ListenedTrackRecordManager {
-	
+
 	public ListenedTrack getListenedTrackFromLine(String line) {
-		
-		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		
-		List<String> values = Arrays.asList( line.split("\t") );
+
+		SimpleDateFormat formatter = new SimpleDateFormat(
+				"yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+		List<String> values = Arrays.asList(line.split("\t"));
 		Iterator<String> iter = values.iterator();
-		
+
 		String code = (iter.hasNext()) ? iter.next() : "";
 		String time = (iter.hasNext()) ? iter.next() : "";
 		String artistCode = (iter.hasNext()) ? iter.next() : "";
 		String artistName = (iter.hasNext()) ? iter.next() : "";
 		String trackCode = (iter.hasNext()) ? iter.next() : "";
 		String trackName = (iter.hasNext()) ? iter.next() : "";
-		
+
 		ListenedTrack listenedTrack = new ListenedTrack();
 		listenedTrack.setCode(code);
-		listenedTrack.setArtistCode((artistCode.equals("")) ? null : artistCode );
-		listenedTrack.setArtistName((artistName.equals("")) ? null : artistName );
-		listenedTrack.setTrackCode((trackCode.equals("")) ? null : trackCode );
-		listenedTrack.setTrackName((trackName.equals("")) ? null : trackName );
-		
-		if(! time.equals("")){
+		listenedTrack.setArtistCode((artistCode.equals("")) ? Encryption
+				.toSHA1(artistName) : artistCode);
+		listenedTrack
+				.setArtistName((artistName.equals("")) ? null : artistName);
+		listenedTrack.setTrackCode((trackCode.equals("")) ? Encryption
+				.toSHA1(trackName) : trackCode);
+		listenedTrack.setTrackName((trackName.equals("")) ? null : trackName);
+
+		if (!time.equals("")) {
 			try {
 				Calendar calendar = new GregorianCalendar();
 				calendar.setTime(formatter.parse(time));
-				listenedTrack.setTime(calendar );
+				listenedTrack.setTime(calendar);
 			} catch (ParseException e) {
 				e.printStackTrace();
-			}	
+			}
 		}
-		
+
 		return listenedTrack;
 	}
 
