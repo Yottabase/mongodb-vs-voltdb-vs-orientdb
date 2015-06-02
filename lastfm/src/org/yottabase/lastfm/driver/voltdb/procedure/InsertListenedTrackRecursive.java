3ded78c7c1dedd97628bded79d5c8c15c211fb4b
Christian Vadalà <chr.vadala@gmail.com>
2015-06-02 17:31:07 +0200

    Aggiunto inserimento di Artist e Track in InsertListenedTrackRecursive

--- lastfm/src/org/yottabase/lastfm/driver/voltdb/procedure/InsertListenedTrackRecursive.java
+++ lastfm/src/org/yottabase/lastfm/driver/voltdb/procedure/InsertListenedTrackRecursive.java
@@ -7,12 +7,28 @@ import org.voltdb.VoltProcedure;
 
 public class InsertListenedTrackRecursive extends VoltProcedure{
 
-	final SQLStmt insert = new SQLStmt(
-            "INSERT INTO ListenedTrack (time, trackCode, userCode) VALUES (?, ?, ?);");
+	final SQLStmt insertListenedTrack = new SQLStmt("INSERT INTO ListenedTrack (time, trackCode, userCode) VALUES (?, ?, ?);");
+	final SQLStmt insertTrack = new SQLStmt("INSERT INTO Track (code, name, artistCode) VALUES (?, ?, ?);");
+	final SQLStmt insertArtist = new SQLStmt("INSERT INTO Artist (code, name) VALUES (?, ?);");
+	
+	final SQLStmt selectTrack = new SQLStmt("SELECT * FROM Track WHERE Code = ? ;");
+	final SQLStmt selectArtist = new SQLStmt("SELECT * FROM Artist WHERE Code = ? ;");
 	
 	public long run (String userCode, Date time, String artistCode, String artistName, String trackCode, String trackName){
 		
-		voltQueueSQL(insert, EXPECT_SCALAR_MATCH(1), time, trackCode, userCode);
+		voltQueueSQL(selectArtist, EXPECT_ZERO_OR_ONE_ROW, trackCode);
+		if(voltExecuteSQL()[0].getRowCount() == 0 ){
+			voltQueueSQL(insertArtist, EXPECT_SCALAR_MATCH(1), trackCode, trackName);
+			voltExecuteSQL();
+		}
+		
+		voltQueueSQL(selectTrack, EXPECT_ZERO_OR_ONE_ROW, trackCode);
+		if(voltExecuteSQL()[0].getRowCount() == 0 ){
+			voltQueueSQL(insertTrack, EXPECT_SCALAR_MATCH(1), trackCode, trackName, artistCode);
+			voltExecuteSQL();
+		}
+		
+		voltQueueSQL(insertListenedTrack, EXPECT_SCALAR_MATCH(1), time, trackCode, userCode);
 		voltExecuteSQL(true);
 		
 		return 0;
