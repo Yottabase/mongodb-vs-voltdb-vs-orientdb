a12df65069218567548fc00bea08b50740678023
alessandroRastelli <alessandro.rastelli89@gmail.com>
2015-06-03 21:32:13 +0200

    index,unique constraint,fix factory

--- lastfm/src/org/yottabase/lastfm/driver/mongodb/MongodbDriver.java
+++ lastfm/src/org/yottabase/lastfm/driver/mongodb/MongodbDriver.java
@@ -6,23 +6,53 @@ import org.yottabase.lastfm.importer.ListenedTrack;
 import org.yottabase.lastfm.importer.User;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
 
 public class MongodbDriver implements Driver {
-	private static MongoDatabase db;
-	private static MongoClient mongoClient;
+	private MongoClient mongoClient;
+	private MongoDatabase db;
 
 	private final static String DATABASE = "lastfm";
 	private final static String COLLECTIONUSERS = "users";
 	private final static String COLLECTIONTRACKS = "tracks";
+	private final static String COLLECTIONARTISTS = "artists";
+
+	private boolean artistDuplicate;
+	private boolean trackDuplicate;
+
+	public MongodbDriver(MongoClient client) {
+		this.mongoClient = client;
+	}
 
 	@Override
 	public void initializeSchema() {
-		mongoClient = new MongoClient();
 		db = mongoClient.getDatabase(DATABASE);
+
 		db.drop();
+
 		System.out.println("crea schema");
+
+		/*
+		 * index options
+		 */
+
+		IndexOptions indOpt = new IndexOptions();
+		indOpt.unique(true);
+
+		// create tracks index
+
+		db.getCollection(COLLECTIONTRACKS).createIndex(
+				new Document("trackId", 1), indOpt);
+
+		// create artist index
+
+		db.getCollection(COLLECTIONARTISTS).createIndex(
+				(new Document("artistId", 1)), indOpt);
+
 	}
 
 	@Override
@@ -38,25 +68,73 @@ public class MongodbDriver implements Driver {
 
 	@Override
 	public void insertListenedTrack(ListenedTrack listenedTrack) {
+		artistDuplicate = false;
+		trackDuplicate = false;
+		FindIterable<Document> iterable;
 
-		db.getCollection(COLLECTIONTRACKS).insertOne(
-				new Document("code", listenedTrack.getCode())
-						.append("time", listenedTrack.getTimeAsJavaDate())
-						.append("artistId", listenedTrack.getArtistCode())
-						.append("artistName", listenedTrack.getArtistName())
-						.append("trackId", listenedTrack.getTrackCode())
-						.append("trackName", listenedTrack.getTrackName()));
+		/*
+		 * insert tracks
+		 */
+		iterable = db.getCollection(COLLECTIONTRACKS).find(
+				new Document("trackId", listenedTrack.getTrackCode()));
 
-		/**
+		iterable.forEach(new Block<Document>() {
+			@Override
+			public void apply(final Document document) {
+				trackDuplicate = true;
+			}
+		});
+
+		if (!trackDuplicate) {
+			db.getCollection(COLLECTIONTRACKS).insertOne(
+					new Document("trackId", listenedTrack.getTrackCode())
+							.append("trackName", listenedTrack.getTrackName()));
+
+		}
+
+		/*
+		 * insert artists
+		 */
+		iterable = db.getCollection(COLLECTIONARTISTS).find(
+				new Document("artistId", listenedTrack.getArtistCode()));
+
+		iterable.forEach(new Block<Document>() {
+			@Override
+			public void apply(final Document document) {
+				artistDuplicate = true;
+			}
+		});
+
+		if (!artistDuplicate) {
+			db.getCollection(COLLECTIONARTISTS)
+					.insertOne(
+							new Document("artistId", listenedTrack
+									.getArtistCode()).append("artistName",
+									listenedTrack.getArtistName()));
+		}
+
+		/*
 		 * associo il brano all'utente che lo ha ascoltato
 		 */
 		BasicDBObject trackObject = new BasicDBObject();
 		trackObject.put("trackId", listenedTrack.getTrackCode());
+		trackObject.put("time", listenedTrack.getTimeAsJavaDate());
 
 		db.getCollection(COLLECTIONUSERS).updateOne(
 				new BasicDBObject("code", listenedTrack.getCode()),
 				new BasicDBObject("$push", new BasicDBObject("tracks",
 						trackObject)));
+
+		/*
+		 * associo il brano all'artista
+		 */
+		BasicDBObject ArtistTracks = new BasicDBObject();
+		ArtistTracks.put("trackId", listenedTrack.getTrackCode());
+
+		db.getCollection(COLLECTIONUSERS).updateOne(
+				new BasicDBObject("code", listenedTrack.getCode()),
+				new BasicDBObject("$push", new BasicDBObject("tracks",
+						ArtistTracks)));
 
 	}
 
--- lastfm/src/org/yottabase/lastfm/driver/mongodb/MongodbDriverFactory.java
+++ lastfm/src/org/yottabase/lastfm/driver/mongodb/MongodbDriverFactory.java
@@ -1,16 +1,34 @@
 package org.yottabase.lastfm.driver.mongodb;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 import org.yottabase.lastfm.core.Driver;
 import org.yottabase.lastfm.core.DriverFactory;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+
 public class MongodbDriverFactory implements DriverFactory {
 
 	@Override
 	public Driver createService(Properties properties) {
-		
-		return new MongodbDriver();
+		Driver driver = null;
+
+		String host = properties.getProperty("mongodb.host");
+		String username = properties.getProperty("mongodb.username");
+		String password = properties.getProperty("mongodb.password");
+
+		MongoCredential credential = MongoCredential.createCredential(username,
+				"lastfm", password.toCharArray());
+
+		MongoClient mongoClient = new MongoClient(
+				new ServerAddress(host, 27017), Arrays.asList(credential));
+
+		driver = new MongodbDriver(mongoClient);
+
+		return driver;
 	}
 
 }
