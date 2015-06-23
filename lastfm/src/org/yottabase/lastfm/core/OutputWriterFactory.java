0607fa67e7d9482d3b53cbbe4c052858eb23c2a5
hduser <hduser@leonardo-K53SD>
2015-06-23 18:05:34 +0200

    aggiunge switch sulla scelta dell'output writer

--- lastfm/src/org/yottabase/lastfm/core/OutputWriterFactory.java
+++ lastfm/src/org/yottabase/lastfm/core/OutputWriterFactory.java
@@ -3,25 +3,23 @@ package org.yottabase.lastfm.core;
 import java.util.Properties;
 
 public class OutputWriterFactory {
-	
-	public OutputWriter createService(Properties properties, String filename){
-		
+
+	public OutputWriter createService(Properties properties, String filename) {
+
 		OutputWriter writer = null;
-		
-		if(properties.getProperty("output.type").equals("filesystem")){
-			
-			String filepath = properties.getProperty("output.folder") + "/" + filename;
-			
-			writer = new TSVFileOutputWriter(filepath);
-			
-		}else{
-			
-			writer = new ConsoleOutputWriter();
-			
+
+		switch (properties.getProperty("output.type")) {
+			case "filesystem":
+				String filepath = properties.getProperty("output.folder") + "/" + filename;
+				writer = new TSVFileOutputWriter(filepath);
+				break;
+
+			default:
+				writer = new ConsoleOutputWriter();
 		}
-		
+
 		return writer;
-		
+
 	}
 
 }
