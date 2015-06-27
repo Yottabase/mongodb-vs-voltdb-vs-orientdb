e61ca66e1a0516ef19497e08e84b95ba19ad14f7
hduser <hduser@leonardo-K53SD>
2015-06-27 16:25:30 +0200

    Code refactoring

--- lastfm/src/org/yottabase/lastfm/core/PropertyFile.java
+++ lastfm/src/org/yottabase/lastfm/core/PropertyFile.java
@@ -33,8 +33,8 @@ public class PropertyFile {
 		}
 	}
 
-	public String get(String eregName) {
-		return this.propertiesFile.getProperty(eregName);
+	public String get(String prop) {
+		return this.propertiesFile.getProperty(prop);
 
 	}
 
